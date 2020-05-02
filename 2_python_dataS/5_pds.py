a45e891fea97cb12cfce10eb337aed69d4572a95
sameep-s <sameep.sharma98@gmail.com>
2020-05-02 12:51:36 +0530

    2nd commit

---
+++ 2_python_dataS/5_pds.py
@@ -0,0 +1,11 @@
+
+fh = open('mbox-short.txt')
+for line in fh:
+	if not line.startswith('From s'): continue
+	print(line.rstrip())
+	words = line.split()
+	email = words[1]
+	print(email)
+	pieces = email.split('@')
+	print(pieces[1])
+	print('\n')
