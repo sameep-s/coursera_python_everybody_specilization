d4c91bef3e2ea223cc0ce7c91f3b34ea3827651a
sameep-s <sameep.sharma98@gmail.com>
2020-05-08 22:30:36 +0530

    'commit'

--- 2_python_dataS/6_pds.py
+++ 2_python_dataS/6_pds.py
@@ -1,8 +1,24 @@
 #Dictonary starting
+'''
 purse = dict()
 purse['money'] = 12
 purse['Candy'] = 3
 purse['Success'] = 100
 print(purse)
+'''
 
-print(purse['Candy'])
+counts = dict()
+names = ['csev', 'cwen', 'zqian', 'cwen']
+for name in names :
+	counts[name] = counts.get(name, 0) + 1
+
+
+	'''#Alternate method
+	if name not in counts :
+		counts[name] = 1
+	else :
+		counts[name] = counts[name] + 1
+	'''
+
+
+print(counts)
