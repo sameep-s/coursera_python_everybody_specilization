a9fb7e8029ef05a75ffbcb09db8302f63a2b0632
sameep-s <sameep.sharma98@gmail.com>
2020-05-09 15:40:17 +0530

    'commit'

--- 2_python_dataS/7_pds.py
+++ 2_python_dataS/7_pds.py
@@ -9,3 +9,8 @@ print('Counting...')
 for word in words:
 	counts[word] = counts.get(word,0) + 1
 print('Counts', counts)
+
+
+names = {'Sameep' : 22, 'Jerusalem' :3022, 'Saleem' : 55}
+for key in names:
+	print(key, names[key])
