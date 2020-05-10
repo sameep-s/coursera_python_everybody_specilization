1e20f5d6558eaae5f0ef1d51aa9a07d621c20c44
dev <dev@localhost.localdomain>
2020-05-11 02:33:38 +0530

    w

--- 2_python_dataS/10_pds.py
+++ 2_python_dataS/10_pds.py
@@ -1,2 +1,19 @@
 #Tuples
 (x, y) = (4, 'ferd')
+d = {'a':10, 'b':1, 'c':22}
+t = sorted(d.items())
+print('This is the value of t: ' ,t)
+print('sorted by key:')
+for k, v in sorted(t):
+	print(k, v)
+
+tmp = list()
+for k, v in d.items():
+	tmp.append((v, k))
+
+print('This statement prints tmp: ', tmp)
+tmp = sorted(tmp, reverse=True)#reverse=true sorts from big value to small.
+print('sorted by value:')
+for k, v in tmp:
+	print(k ,v)
+
---
+++ 2_python_dataS/11_pds.py
@@ -0,0 +1,15 @@
+fhand = open('mbox-short.txt')
+counts = dict()
+for line in fhand:
+	words = line.split()
+	for word in words:
+		counts[word] = counts.get(word, 0) + 1
+
+lst = list()
+for key, val in counts.items():
+	newtup = (val, key)
+	lst.append(newtup)
+
+lst = sorted(lst, reverse=True)
+for val, key in lst[:10]:
+	print('Keys: ', key, '  ,Values/Times occured: ', val)
