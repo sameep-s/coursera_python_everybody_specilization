1d19dbd6df4b156832eb29f973fb7bbe3baa287d
dev <dev@localhost.localdomain>
2020-05-10 20:30:47 +0530

    assignment 9.4 complete

--- 2_python_dataS/8_pds.py
+++ 2_python_dataS/8_pds.py
@@ -4,8 +4,7 @@ handle = open(name)
 counts = dict()
 for line in handle:
 	words = line.split()
-	for word in words:
-		counts[word] = counts.get(word,0) + 1
+	counts[word] = counts.get(word,0) + 1
 
 bigCount = None
 bigWord = None
--- 2_python_dataS/9_pds.py
+++ 2_python_dataS/9_pds.py
@@ -4,17 +4,19 @@ if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
 
 counts = dict()
-
+print('---------------------------------------------------------------------------------')
 for line in handle:
     line = line.rstrip()
-    if not line.startswith('From '): continue
+    if not line.startswith('From:'): continue
     words = line.split()
-    for word in words:
-        counts[word] = counts.get(word,0) + 1
+    words = words[1]
+    print(words)
+    counts[words] = counts.get(words, 0) + 1
+print('---------------------------------------------------------------------------------')
+
           
 bigcount = None
 bigword = None
-print(counts.items())
            
 for word,count in counts.items():
 	if bigcount is None or count > bigcount:
